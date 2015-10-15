b2ad736a6b7789a750900cfe53a2933bc3ee3edc
Robert White <ai.robert.wangshuai@gmail.com>
2015-10-15 11:28:40 +0200

    still confused

--- SMCDEL/KNSSMT.hs
+++ SMCDEL/KNSSMT.hs
@@ -33,7 +33,7 @@ module KNSSMT where
   boolSMTOf :: Form -> ExpY
   boolSMTOf Top           = true
   boolSMTOf Bot           = false
-  boolSMTOf (PrpF (P n))  = VarE (show n)
+  boolSMTOf p@(PrpF (P n))  = VarE (show n)
   boolSMTOf (Neg forms)    = NOT (boolSMTOf forms)
   boolSMTOf (Conj forms)  = AND (map boolSMTOf forms)
   boolSMTOf (Disj forms)  = OR (map boolSMTOf forms)
@@ -57,13 +57,17 @@ module KNSSMT where
 
 -- not that the boolEval in SMCDEL is then going to be a call to Yices with assignment given
 -- declaration from Prp to CmdY
+
+  name :: Int -> String
+  name x = "p_" ++ (show x)
+
   defs :: [Prp] -> [CmdY]
   defs sl = 
-    map (\(P i) -> DEFINE (("p_" ++ (show i)), bool) Nothing) sl
+    map (\(P i) -> DEFINE ((name i), bool) Nothing) sl
 
   vars :: [Prp] -> [ExpY]
   vars sl =
-    map (\(P i) -> VarE ("p_" ++ (show i))) sl
+    map (\(P i) -> VarE (name i)) sl
 
 
   boolEval :: [Prp] -> Form -> IO Bool 
@@ -103,14 +107,55 @@ module KNSSMT where
   test_truth = do 
     result <- (boolEval [P 1, P 2] test_1)
     if result then print "yes!"
-      else print "oh no!"
+      else (print "oh no!")
 
--- instead of BDD, we introduce a new datastructure for (propsitional variables, clauses)
-type BddY = [CmdY]
+  -- instead of BDD, we introduce a new datastructure for (propsitional variables, clauses)
+  type BddY = [CmdY]
 
-data KnowStructY = KnS [Prp] BddY [(Agent,[Prp])] deriving (Eq,Show)
-type KnState = [Prp]
-type ScenarioY = (KnowStructY, KnState)
+  data KnowStructY = KnS [Prp] BddY [(Agent,[Prp])] deriving (Show)
+  type KnState = [Prp]
+  type ScenarioY = (KnowStructY, KnState)
 
 
 
+-- this is the main function I need to change
+bddOf :: KnowStruct -> Form -> ExpY
+bddOf _   Top           = true
+bddOf _   Bot           = false
+bddOf _   p@(PrpF (P n))  = boolSMTOf p
+bddOf kns p@(Neg form)    = boolSMTOf p
+bddOf kns p@(Conj forms)  = boolSMTOf p
+bddOf kns p@(Disj forms)  = boolSMTOf p
+bddOf kns p@(Xor  forms)  = boolSMTOf p
+bddOf kns p@(Impl f g)    = boolSMTOf p
+bddOf kns p@(Equi f g)    = boolSMTOf p
+bddOf kns p@(Forall ps f) = boolSMTOf p
+bddOf kns p@(Exists ps f) = boolSMTOf p
+bddOf kns@(KnS allprops lawbdd obs) (K i form) =
+  FORALL otherps (bddOf kns form) where
+    otherps = map (\(P n) -> (name n, bool)) $ allprops \\ apply obs i
+    -- agent i knows wether ... I find this hard to understand. why do we introduce this?
+bddOf kns@(KnS allprops lawbdd obs) (Kw i form) =
+  OR [FORALL otherps (bddOf kns f)) | f <- [form, Neg form]] where
+    otherps = map (\(P n) -> (name n, bool)) $ allprops \\ apply obs i
+-- common knowledge. that's the confusing part
+bddOf kns@(KnS allprops lawbdd obs) (Ck ags form) = gfp lambda where
+  lambda z = AND $ (bddOf kns form) : [ forallSet (otherps i) (imp lawbdd z) | i <- ags ]
+  otherps i = map (\(P n) -> n) $ allprops \\ apply obs i
+  -- 
+bddOf kns (Ckw ags form) = dis (bddOf kns (Ck ags form)) (bddOf kns (Ck ags (Neg form)))
+bddOf kns@(KnS props _ _) (Announce ags form1 form2) =
+  imp (bddOf kns form1) (restrict bdd2 (k,True)) where
+    bdd2  = bddOf (announce kns ags form1) form2
+    (P k) = freshp props
+bddOf kns@(KnS props _ _) (AnnounceW ags form1 form2) =
+  ifthenelse (bddOf kns form1) bdd2a bdd2b where
+    bdd2a = restrict (bddOf (announce kns ags form1) form2) (k,True)
+    bdd2b = restrict (bddOf (announce kns ags form1) form2) (k,False)
+    (P k) = freshp props
+bddOf kns (PubAnnounce form1 form2) = imp (bddOf kns form1) newform2 where
+    newform2 = bddOf (pubAnnounce kns form1) form2
+bddOf kns (PubAnnounceW form1 form2) =
+  ifthenelse (bddOf kns form1) newform2a newform2b where
+    newform2a = bddOf (pubAnnounce kns form1) form2
+    newform2b = bddOf (pubAnnounce kns (Neg form1)) form2
